dc5a05d9b340c3673207e5e927440f308a16a628
William S <will.step.205@gmail.com>
2023-06-02 10:31:07 -0700

    single upload logic added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.listen(port, function () {
   console.log('Your app is listening on port ' + port);
 });
 
+// middleware to store files in a folder
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads');
@@ -32,8 +33,9 @@ const fileStorageEngine = multer.diskStorage({
 const uploads = multer({ storage: fileStorageEngine })
 
 app.post("/api/fileanalyse", uploads.single('upfile'), async (req,res) => { // for single file
-// app.post("/api/fileanalyse", uploads.array('upfile', 2), async (req, res) => { // for 2 files
+// app.post("/api/fileanalyse", uploads.array('upfile'), async (req, res) => { // for multiple files, don't forget to add multiple next to name="upfile" in index.html
   // console.log(req.file);
-  // res.send("Upload complete");
-  res.json({ name: req.file.filename, type: req.file.mimetype, size: req.file.size, });
+  // console.log(req.files);
+  res.json({ name: req.file.filenames, type: req.file.mimetypes, size: req.file.sizes, });
+    // res.send({uploaded: req.files});
 });
