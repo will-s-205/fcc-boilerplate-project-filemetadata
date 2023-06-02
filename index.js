10c983f8637d3d51bd7a993011e70d0fa3bdcd96
will-s-205 <william.step.0205@gmail.com>
2023-06-02 22:20:29 +0000

    final changes

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port);
 });
 
