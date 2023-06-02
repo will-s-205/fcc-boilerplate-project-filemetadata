ada12985c478643641e255e09128f66443bdf27c
William S <will.step.205@gmail.com>
2023-06-02 08:45:49 -0700

    body parser added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
